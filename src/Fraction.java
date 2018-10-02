4e880e8fa2823bb5ebba85c2eb9c734c91c050ad
t00202160 <paul.cafferkey@ittralee.ie>
2018-10-02 17:28:34 +0100

    done

--- src/Fraction.java
+++ src/Fraction.java
@@ -36,7 +36,46 @@ public class Fraction {
 
     public Fraction add(Fraction fraction2){
 
-        
+
+        int num1 = (this.numerator * fraction2.denominator)+ (fraction2.numerator * this.denominator);
+        int denom1 = (this.denominator * fraction2.denominator);
+
+        Fraction answer = new Fraction(num1,denom1);
+
+        return answer;
+
+    }
+
+    public Fraction subtract(Fraction fraction2){
+
+
+        int num1 = (this.numerator * fraction2.denominator) - (fraction2.numerator * this.denominator);
+        int denom1 = (this.denominator * fraction2.denominator);
+
+        Fraction answer = new Fraction(num1,denom1);
+
+        return answer;
+
+    }
+
+    public Fraction multiply(Fraction fraction2){
+        int num1 = (this.numerator * fraction2.numerator);
+        int denom1 = (this.denominator * fraction2.denominator);
+
+        Fraction answer = new Fraction(num1,denom1);
+
+        return answer;
+
+
+    }
+
+    public Fraction divide(Fraction fraction2){
+        int num1 = (this.numerator * fraction2.denominator);
+        int denom1 = (this.denominator * fraction2.numerator);
+
+        Fraction answer = new Fraction(num1,denom1);
+
+        return answer;
 
 
     }
---
+++ src/FractionDriver.java
@@ -0,0 +1,30 @@
+import java.sql.SQLOutput;
+
+public class FractionDriver {
+    public static void main(String[] args) {
+
+        Fraction fraction = new Fraction(15,25);
+        Fraction fraction2 = new Fraction(10,20);
+        Fraction fraction3 = new Fraction();
+        Fraction fraction4= new Fraction();
+        Fraction fraction5= new Fraction();
+        Fraction fraction6= new Fraction();
+
+        System.out.println(fraction.toString());
+        fraction3 = fraction.add(fraction2);
+        System.out.println(fraction3);
+
+        fraction4 = fraction.subtract(fraction2);
+        System.out.println(fraction4);
+
+        fraction5 = fraction.multiply(fraction2);
+        System.out.println(fraction5);
+
+        fraction6 = fraction.divide(fraction2);
+        System.out.println(fraction6);
+
+
+
+
+    }
+}
