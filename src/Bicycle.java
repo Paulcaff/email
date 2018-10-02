4065f653af2558ab68fa240cc798a54d3dc5136a
t00202160 <paul.cafferkey@ittralee.ie>
2018-10-02 17:56:29 +0100

    done

---
+++ src/Bicycle.java
@@ -0,0 +1,45 @@
+public class Bicycle {
+
+    private String name;
+    private double value;
+    private String make;
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String toString(){
+        return "Name: "+name+"\nValue: "+"\nMake: "+make;
+    }
+
+    public Bicycle() {
+        this.name = "None";
+        this.value = 0;
+        this.make = "None";
+    }
+
+    public Bicycle(String name,String make,Double value){
+        setName(name);
+        setMake(make);
+        setValue(value);
+    }
+}
---
+++ src/BicycleDriver.java
@@ -0,0 +1,6 @@
+public class BicycleDriver {
+
+    public static void main(String[] args) {
+        
+    }
+}
